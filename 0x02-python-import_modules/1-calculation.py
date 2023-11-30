0592a0b94b970365988a07eb8d0b1fe54fc2f8eb
Dennis Adigwe <adigwedennis@gmail.com>
2023-11-30 13:24:44 +0100

    updated

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-if if __name__ == "__main__":
+if  __name__ == "__main__":
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
