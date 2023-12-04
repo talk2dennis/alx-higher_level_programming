c6d791fd1791dbab22f9c9d61c30ddc6b0fb910a
Dennis Adigwe <adigwedennis@gmail.com>
2023-12-04 17:39:52 +0100

    reversed to allocating memory dynamically

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,22 +10,22 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *current;
-	int start = 0, end = 0, buffer[1024];
+	int start = 0, end = 0, *buffer = NULL;
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
 	current = *head;
-	/**while (current != NULL)
+	while (current != NULL)
 	{
 		current = current->next;
 		end++;
 	}
-	buffer = malloc(sizeof(int) * end);
+	buffer = malloc(sizeof(int) *end);
 	if (buffer == NULL)
 		return (0);
 	end = 0;
-	current = *head;*/
+	current = *head;
 	while (current != NULL)
 	{
 		buffer[end] = current->n;
@@ -36,10 +36,12 @@ int is_palindrome(listint_t **head)
 	{
 		if (buffer[start] != buffer[end - 1])
 		{
+			free(buffer);
 			return (0);
 		}
 		start++;
 		end--;
 	}
+	free(buffer);
 	return (1);
 }
