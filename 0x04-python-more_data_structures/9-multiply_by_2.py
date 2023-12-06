e1df9b13be0b69f409cba994a94e08b569584fa3
Dennis Adigwe <adigwedennis@gmail.com>
2023-12-06 18:36:28 +0100

    made the lambda line shorter

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    return dict(map(lambda key_value: (key_value[0], key_value[1] * 2), a_dictionary.items()))
+    tup = a_dictionary.items()
+    return dict(map(lambda key_value: (key_value[0], key_value[1] * 2), tup))
