303220c46617b4d787d01a2ebfb2a758149e3965
Dennis Adigwe <adigwedennis@gmail.com>
2023-12-07 10:56:23 +0100

    updated task 12

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda x: x * x, row)) for row in matrix]
+    return (list(map(lambda x: list(map(lambda y: y ** 2, x[:])), matrix)))
