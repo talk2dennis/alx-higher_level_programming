ef6edb57d03b4d30a39f864e10b2f430ab6b4dae
Dennis Adigwe <adigwedennis@yahoo.com>
2023-12-18 05:00:25 -0800

    completed task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    num = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]))
+            num += 1
+        except (ValueError, TypeError):
+            continue
+    return num
 
