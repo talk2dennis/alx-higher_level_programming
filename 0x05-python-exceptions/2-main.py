c9afbbd4f333d9d6b65f5fb7923dbc959e8c6864
Dennis Adigwe <adigwedennis@yahoo.com>
2023-12-18 05:13:57 -0800

    added print formating

--- 0x05-python-exceptions/2-main.py
+++ 0x05-python-exceptions/2-main.py
@@ -1,11 +1,14 @@
-#!/usr/bin/python3
-safe_print_list = __import__('0-safe_print_list').safe_print_list
+afe_print_list_integers = \
+    __import__('2-safe_print_list_integers').safe_print_list_integers
 
 my_list = [1, 2, 3, 4, 5]
 
-nb_print = safe_print_list(my_list, 2)
+nb_print = safe_print_list_integers(my_list, 2)
 print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,10 @@ def safe_print_list_integers(my_list=[], x=0):
     num = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]))
+            print("{:d}".format(my_list[i]), end="")
             num += 1
         except (ValueError, TypeError):
             continue
+    print("")
     return num
 
