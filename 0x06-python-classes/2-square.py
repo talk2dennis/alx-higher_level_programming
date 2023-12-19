374728406f5a6adff84ab9fa9235b61d42823d43
Dennis Adigwe <adigwedennis@yahoo.com>
2023-12-19 05:54:00 -0800

    fix typo erro

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -19,4 +19,4 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = sizee
+        self.__size = size
