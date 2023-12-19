dc0eafbcb07b1d5920b873b03c5a138371597e45
Dennis Adigwe <adigwedennis@yahoo.com>
2023-12-19 11:58:30 -0800

    completed task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Defines a class Square
+
+"""
+
+
+class Square:
+    """ Square is a class with private instance atrinbue of size"""
+
+    def __init__(self, size=0):
+        """
+        creates a new instance of a Square
+        Args:
+            size: size of the square
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        defines a method that returns the area of a square
+        Return: The area of the square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        defines getter for size
+        Return: size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        defines setter for private instance of size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
