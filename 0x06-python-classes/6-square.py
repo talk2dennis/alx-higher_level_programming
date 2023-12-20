55f134f9c1b7fb055e6787888674ecfad542a135
Dennis Adigwe <adigwedennis@gmail.com>
2023-12-20 05:16:17 +0100

    checked for edge cases

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -68,7 +68,9 @@ class Square:
         defines a setter for ppsition
         """
 
-        if not isinstance(value, tuple) or len(value) != 2:
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if not isinstance(value[0], int) or not isinstance(value[1], int):
             raise TypeError("position must be a tuple of 2 positive integers")
