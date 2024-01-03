b804a6c213325ec827af4e037cc36e1022045860
Dennis Adigwe <adigwedennis@gmail.com>
2024-01-03 13:06:59 +0100

    completed task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+
+def print_square(size):
+    """
+    print_square - A function that prints squares using #
+    Args:
+        size: is the size length of the square (int)
+    raises:
+        TypeError: size must be an integer
+        ValueError: size must be >= 0
+        TypeError: if size is float and less than 0
+    Return: nothing
+    """
+
+    if not isinstance(size, int) or (isinstance(size, float) and size < 0):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size, end="")
+        print()
