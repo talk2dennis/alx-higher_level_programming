623238c03ce2511efdb2fae21d23f3055b93e67b
Dennis Adigwe <adigwedennis@gmail.com>
2024-01-04 23:10:25 +0100

    fixed code failure

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -90,8 +90,8 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         return rect_1 if rect_1.area() >= rect_2.area() else rect_2
 
-        @classmethod
-        def square(cls, size=0):
+    @classmethod
+    def square(cls, size=0):
         """
         to create a new instance of square from the rectangle class
         Args:
