ded741cff92bc3fb36d3188ce6fa9a89070eb443
Dennis Adigwe <adigwedennis@gmail.com>
2024-01-08 21:30:28 +0100

    completed task3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""funtions return true if an obj is an instance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns true if an obj is an instance of a class otherwise false"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        False
