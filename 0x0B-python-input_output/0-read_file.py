ed109da8faac68131130b258bdb868fa31eaf586
Dennis Adigwe <adigwedennis@gmail.com>
2024-01-09 13:19:00 +0100

    opened file with unicode format

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """read_file - a function that reads from a file"""
-    with open(filename) as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end='')
