c3bdbeaeb06f89006e22b52808c7e4c2d24036b0
Adigwe Dennis <63774376+talk2dennis@users.noreply.github.com>
2024-01-09 13:37:34 +0100

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """a function that writes to a file"""
-    with open(filename, w, encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
