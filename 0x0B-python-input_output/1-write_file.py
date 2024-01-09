74430f6d0562085b4fedad6077264fe933b5da72
Dennis Adigwe <adigwedennis@gmail.com>
2024-01-09 13:35:16 +0100

    completed task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""write to a file"""
+
+
+def write_file(filename="", text=""):
+    """a function that writes to a file"""
+    with open(filename, w, encoding="utf-8") as f:
+        return f.write(text)
