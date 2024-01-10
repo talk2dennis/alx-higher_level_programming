e5e3329f6dcad5bf8f99af131291b05b6eba7685
Dennis Adigwe <adigwedennis@gmail.com>
2024-01-10 18:35:02 +0100

    completed task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+student class
+Arthor: Adigwe Dennis
+"""
+class_to_json = __import__('8-class_to_json').class_to_json
+
+
+class Student:
+    """
+    Difines class Student
+    Args:
+        first_name (str): first_name of the student
+        last_name (str): last name of student
+        age (int): age of student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+        return class_to_json(self)
