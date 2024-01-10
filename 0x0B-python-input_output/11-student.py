a3e7584b8d487d803b8311bf7f03cfe79ff28b2b
Dennis Adigwe <adigwedennis@gmail.com>
2024-01-10 19:18:26 +0100

    completed task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+student class
+Arthor: Adigwe Dennis
+"""
+
+
+class Student:
+    """
+    Defines class Student
+    Args:
+        first_name (str): first_name of the student
+        last_name (str): last name of student
+        age (int): age of student
+    """
+    def __init__(self, first_name, last_name, age):
+        """initialises the Student class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Convert object attributes to a dictionary"""
+        obj = vars(self).copy()
+        if type(attrs) is list:
+            filtered_obj = {}
+            for attr in attrs:
+                if type(attr) is not str:
+                    return obj
+                if attr in obj:
+                    filtered_obj[attr] = obj[attr]
+            return filtered_obj
+        return obj
+
+    def reload_from_json(self, json):
+        """reloads data from json object"""
+        for item in json:
+            vars(self)[item] = json[item]
