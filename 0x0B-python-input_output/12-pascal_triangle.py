eba2232939f0df0b68cdba8364692707a969e9fb
Dennis Adigwe <adigwedennis@gmail.com>
2024-01-10 19:34:08 +0100

    completed task 12-pascal_triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""pascal triangle"""
+
+
+def pascal_triangle(n):
+    """implements pascal triangle"""
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+        return triangle
