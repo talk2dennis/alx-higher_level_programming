b6932deab264f0fa6372d5bc8df23d7e39201cdb
Dennis Adigwe <adigwedennis@gmail.com>
2024-02-14 15:06:36 +0100

    created sql query with sub quries

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- Write a script that lists all the cities of California that can
+--	be found in the database hbtn_0d_usa.
+-- The states table contains only one record where name = California
+--	(but the id can be different, as per the example)
+-- Results must be sorted in ascending order by cities.id
+SELECT id, name FROM cities WHERE state_id
+	IN (SELECT id FROM states WHERE name = 'California');
