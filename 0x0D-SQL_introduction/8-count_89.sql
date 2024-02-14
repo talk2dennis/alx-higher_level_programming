5be26ddbc1fd8c84d07adba5cc695a3e5197c366
Dennis Adigwe <adigwedennis@gmail.com>
2024-02-14 09:32:20 +0100

    id` value of updated count_89

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,3 @@
--- count all rows where id = 98 in a table
-SELECT COUNT(*) FROM first_table WHERE id = 98;
+-- a script that displays the number of records with id = 89
+--	in the table first_table of the database hbtn_0c_0 in your MySQL server.
+SELECT COUNT(*) FROM first_table WHERE id = 89;
