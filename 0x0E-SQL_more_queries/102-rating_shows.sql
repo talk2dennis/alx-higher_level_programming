a7db09002eac9081cc89e418a688dea3adb8e1cc
Dennis Adigwe <adigwedennis@yahoo.com>
2024-02-15 06:28:48 -0800

    completed task 103

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,11 +1,9 @@
 -- Write a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
 -- Each record should display: tv_shows.title - rating sum
 -- Results must be sorted in descending order by the rating
-SELECT `tv_genres`.`name`, SUM(`tv_show_ratings`.`rate`) AS `rating`
-	FROM `tv_genres`
-	INNER JOIN `tv_show_genres`
-	ON `tv_show_genres`.`genre_id` = `tv_genres`.`id`
+SELECT `title`, SUM(`rate`) AS `rating`
+	FROM `tv_shows`
 	INNER JOIN `tv_show_ratings`
-	ON `tv_show_ratings`.`show_id` = `tv_show_genres`.`show_id`
-	GROUP BY `tv_genres`.`name`
-	ORDER BY `rating` DESC;
+	ON `tv_shows`.`id` = `tv_show_ratings`.`show_id`
+	GROUP BY `title`
+ 	ORDER BY `rating` DESC;
