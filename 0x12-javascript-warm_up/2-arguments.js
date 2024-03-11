8ea83b7dc57274e7e34b64c5ad190a2a683cd9e5
talk2dennis <adigwedennis@gmail.com>
2024-03-11 19:39:01 +0100

    completed task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
+const { argv } = require('process');
 
-if (argv.lenght < 2)
+if (argv.length < 3)
 {
 	console.log('No argument');
 } else
