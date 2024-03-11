739ea71c03d3d68a616f010db074ebb8f454c224
talk2dennis <adigwedennis@gmail.com>
2024-03-11 22:12:41 +0100

    completed task 13 - exports add

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+exports.add = function (a, b) {
+  return a + b;
+};
+
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,6 +4,6 @@ const { argv } = require('process');
 
 function add(a, b) {
   return parseInt(a) + parseInt(b);
-}
+};
 
 console.log(add(argv[2], argv[3]));
