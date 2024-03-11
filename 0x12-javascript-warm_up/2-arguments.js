6f927babe6cda116e2ebf2de9b94ac30a642b0bb
talk2dennis <adigwedennis@gmail.com>
2024-03-11 20:58:46 +0100

    fixed semistandard style

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,13 +2,10 @@
 
 const { argv } = require('process');
 
-if (argv.length < 3)
-{
-	console.log('No argument');
-} else if (argv.length == 3)
-{
-	console.log('Argument found');
-} else
-{	
-	console.log('Arguments found');
+if (argv.length < 3) {
+  console.log('No argument');
+} else if (argv.length == 3) {
+  console.log('Argument found');
+} else {	
+  console.log('Arguments found');
 }
