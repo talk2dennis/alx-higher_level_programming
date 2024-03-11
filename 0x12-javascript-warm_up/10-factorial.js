c90288e619e266819e4566e767a12f7a9412dcaf
talk2dennis <adigwedennis@gmail.com>
2024-03-11 22:22:34 +0100

    completed task 10 - factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// factorial using recursion
+
+const { argv } = require('process');
+
+function factorial (num) {
+  if (isNaN(num) || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num -1);
+  }
+}
+
+console.log(factorial(parseInt(argv[2])));
