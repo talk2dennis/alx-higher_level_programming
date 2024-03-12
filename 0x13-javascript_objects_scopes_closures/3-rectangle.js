3209029db8e6954ef8133412c434e9f6156b748f
talk2dennis <adigwedennis@gmail.com>
2024-03-12 20:27:54 +0100

    completed task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// defines an empty rectangle class
+
+class Rectangle {
+  constructor (w, h) {
+    // creates an empty class if w or h <= 0
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Rectangle;
