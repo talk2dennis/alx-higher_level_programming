49ed980949c5be08f544470b2eba0ddaa113379a
talk2dennis <adigwedennis@gmail.com>
2024-03-13 03:24:32 +0100

    started sorted dict

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence
+
+const { dict } = require('./101-data').dict;
+
+
