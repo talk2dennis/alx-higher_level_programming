86a4335719e204898e186b7da488d8b1e71cc40f
talk2dennis <adigwedennis@gmail.com>
2024-03-13 10:40:29 +0100

    implemented sorting of js object using key

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,15 @@
 #!/usr/bin/node
 // script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence
 
-const { dict } = require('./101-data').dict;
+const { dict } = require('./101-data');
 
-
+const newDict = {};
+for (const id in dict) {
+  console.log(id);
+  if (newDict[dict[id]]) {
+    newDict[dict[id]].push(id);
+  } else {
+    newDict[dict[id]] = [id];
+  }
+}
+console.log(newDict);
