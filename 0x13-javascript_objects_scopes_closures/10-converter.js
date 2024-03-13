e9779e667a8016bdda10f0295e32737aa3ae8dc2
talk2dennis <adigwedennis@gmail.com>
2024-03-13 02:20:43 +0100

    implemented higher order function

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// onverts a number from base 10 to another base passed as argument
+
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
+  };
+};
