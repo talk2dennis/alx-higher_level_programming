d41a968c6b74877877fa903985c25065e6d31958
talk2denis <adigwedennis@yahoo.com>
2024-03-15 06:08:16 -0700

    added binary to force case sensivity

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
                 ORDER BY id ASC")
     rows = cur.fetchall()
     for state in rows:
