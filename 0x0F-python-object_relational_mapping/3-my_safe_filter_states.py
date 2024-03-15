5594b445e37e94de04cc5ccf134641741286b668
talk2denis <adigwedennis@yahoo.com>
2024-03-15 06:33:53 -0700

    used place holder for my queries

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
-                ORDER BY id ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s\
+                ORDER BY id ASC", (argv[4],))
     rows = cur.fetchall()
     for state in rows:
         print(state)
