fe7a6593e08b821ed47a617d0f816954fdd349e9
talk2dennis <adigwedennis@gmail.com>
2024-03-16 16:40:05 +0100

    used sqlalchemy to query database

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -20,5 +20,6 @@ class State(Base):
     """
 
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                unique=True, nullable=False)
     name = Column(String(128), nullable=False)
