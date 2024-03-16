99d8d1d751359c05f28dc74dfb074ded6300a428
talk2dennis <adigwedennis@gmail.com>
2024-03-16 04:11:58 +0100

    mapped python class to mysqldb using sqlalchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,7 +9,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name =  Column(String(128), nullable=False, unique=True)
+    name = Column(String(128), nullable=False, unique=True)
