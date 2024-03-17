83794f2b3f423fa7e9a867f5cf476e66f70224f7
talk2dennis <adigwedennis@gmail.com>
2024-03-17 03:54:52 +0100

    added relationship to the model

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+ a python file that contains the class definition of a City and
+    an instance Base = declarative_base()
+"""
+
+from model_state import State, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class City(Base):
+    """
+    City: inherits from Base declarative class
+           links to the MySQL table states
+    Args:
+        id (int): the id of the state
+        states.id (int): foreign key to State
+        name (string): name of the state
+    """
+
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+ a python file that contains the class definition of a State and
+    an instance Base = declarative_base()
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State: inherits from Base declarative class
+           links to the MySQL table states
+    Args:
+        id (int): the id of the state
+        name (string): name of the state
+    """
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = Relationship("City", backref="state", cascade="delete")
