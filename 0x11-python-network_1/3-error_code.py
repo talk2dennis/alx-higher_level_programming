c17f6adac889d37be4190166fa8043db29bef3db
talk2dennis <adigwedennis@gmail.com>
2024-03-28 21:26:07 +0100

    a request to get error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Python script that takes in a URL and an email, sends a POST request to the
+    passed URL with the email as a parameter, and displays the
+        body of the response (decoded in utf-8)
+"""
+
+if __name__ == '__main__':
+    from sys import argv
+    from urllib import error, request
+    
+
+    url = argv[1]
+    try:
+        with request.urlopen(url) as res:
+            print(res.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print(e.code)
