26a8c87059fe7beae95e8e2b6f7e8e8b31328d97
talk2dennis <adigwedennis@gmail.com>
2024-03-28 20:40:31 +0100

    fixed for pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,6 @@ if __name__ == '__main__':
     with urllib.request.urlopen(url) as response:
         html = response.read()
         print("Body response:")
-        print(f"\t- {type(html)}")
-        print(f"\t- {html}")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
         print(f"\t- utf8 content: {html.decode('utf-8')}")
-
