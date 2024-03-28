2737acff9fac5f61c4b7358a5649149ddee87bd5
talk2dennis <adigwedennis@gmail.com>
2024-03-28 22:13:03 +0100

    print statuse code for requests error

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Python script that takes in a URL and an email address, sends a POST reques
+    to the passed URL with the email as a parameter, and finally displays the
+    body of the response.
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    url = argv[1]
+
+    try:
+        res = requests.get(url)
+        print(res.text)
+    except requests.RequestException as e:
+        print(e)
