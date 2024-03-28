d52a81bf5a2621059fb27e94dbec56df8da53d6e
talk2dennis <adigwedennis@gmail.com>
2024-03-28 21:57:34 +0100

    used requests to get the response headers

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    a Python script that fetches https://alx-intranet.hbtn.io/status
+    use the package requests
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    res = requests.get(url)
+    print(f"{res.headers['X-Request-Id']}")
