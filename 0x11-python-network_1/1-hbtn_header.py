754d4e83e8663418da538e8e04e3e76b8b85bcb0
talk2dennis <adigwedennis@gmail.com>
2024-03-28 20:47:38 +0100

    used request.Request

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    a Python script that takes in a URL, sends a request to the URL
+    and displays the value of the X-Request-Id
+"""
+
+if __name__ == '__main__':
+    from sys import argv
+    import urllib.request
+    req = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(req) as res:
+        print(f"\t- res {res}")
